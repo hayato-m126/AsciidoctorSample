84ac07c536af09035cecdcdacf2730fdee938bb6
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2019-01-22 01:25:03 +0900

    サンプルの表紙を追加、表紙をサンプルの表紙に置き換えてPDFを生成するコマンドに変更

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,1 +1,1 @@
-asciidoctor-pdf sample.adoc -o sample.pdf -n -r asciidoctor-diagram
+asciidoctor-pdf sample.adoc -o sample.pdf -a notitle -a front-cover-image=image:sample_cover.pdf[] -n -r asciidoctor-diagram
